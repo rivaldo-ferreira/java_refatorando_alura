5fe31241cc3e3efe54652b33b195f3a9c8b13603
Rivas <rivaldo13@gmail.com>
2023-10-03 14:44:12 -0300

    Projeto Refactor Java Alura

---
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -0,0 +1,53 @@
+package br.com.alura.domain;
+
+public class Pet {
+
+    private long id;
+    private String tipo;
+    private String nome;
+    private String raca;
+    private int idade;
+    private String cor;
+    private float peso;
+
+    public Pet(){
+
+    }
+
+    public Pet(String tipo, String nome, String raca, int idade, String cor, float peso) {
+        this.tipo = tipo;
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.cor = cor;
+        this.peso = peso;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+}
